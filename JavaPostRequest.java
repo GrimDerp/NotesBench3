52bd6e21e20f3731eb7e1e36a3f69af26dfb98d4
TheGrimDerp <admin@superzen.org>
2018-10-16 03:35:16 -0600

    Working POST

---
+++ JavaPostRequest.java
@@ -0,0 +1,53 @@
+//maybe a package
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class JavaPostRequest {
+  private static HttpURLConnection con;
+
+  public static void main(String[] args) throws MalformedURLException, ProtocolException, IOException {
+    String url = "httpss://superzen.org";
+    String urlParameters = "name=TheGrimDerp&occupation=Adjustor";
+    byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+
+    try {
+      URL myurl = new URL(url);
+      con = (HttpURLConnection) myurl.openConnection();
+
+      con.setDoOutput(true);
+      con.setRequestMethod("POST");
+      con.setRequestProperty("User-Agent", "Java client");
+      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+            wr.write(postData);
+        }
+
+        StringBuilder content;
+
+        try (BufferedReader in = new BufferedReader(
+          new InputStreamReader(con.getInputStream()))) {
+
+            String line;
+            content = new StringBuilder();
+
+            while ((line = in.readLine()) !=null) {
+              content.append(line);
+              content.append(System.lineSeparator());
+            }
+          }
+        System.out.println(content.toString());
+    }
+    finally{
+      con.disconnect();
+    }
+  }
+}
